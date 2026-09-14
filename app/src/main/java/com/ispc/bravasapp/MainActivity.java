edc545a7bdabdf7927761f08eef7ca4598285ef2
Sole <scarlatamarias@gmail.com>
2026-09-14 16:45:24 -0300

    Commit inicial del proyecto

---
+++ app/src/main/java/com/ispc/bravasapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ispc.bravasapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+
+        btnCerrarSesion.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
